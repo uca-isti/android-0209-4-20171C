57091e76d5d7e352fdc70b1315b06acd929307db
Darsy Rodriguez <darsyflores201040@gmail.com>
2017-04-17 16:48:07 -0600

    Se agrega Modelo de Carrera y el método Get en ApiInterface

--- app/src/main/java/uca/apps/isi/Auapp/api/ApiInterface.java
+++ app/src/main/java/uca/apps/isi/Auapp/api/ApiInterface.java
@@ -5,6 +5,7 @@ import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import uca.apps.isi.Auapp.models.Asignatura;
+import uca.apps.isi.Auapp.models.Carrera;
 
 /**
  * Created by isi3 on 17/4/2017.
@@ -14,5 +15,8 @@ public interface ApiInterface {
 
     @GET("asignaturas")
     Call<List<Asignatura>> getAsignaturas();
+
+    @GET("carreras")
+    Call<List<Carrera>> getCarreras();
 
 }
---
+++ app/src/main/java/uca/apps/isi/Auapp/models/Carrera.java
@@ -0,0 +1,27 @@
+package uca.apps.isi.Auapp.models;
+
+/**
+ * Created by isi3 on 17/4/2017.
+ */
+
+public class Carrera {
+
+    private int id_carrera;
+    private String nombre;
+
+    public int getId_carrera() {
+        return id_carrera;
+    }
+
+    public void setId_carrera(int id_carrera) {
+        this.id_carrera = id_carrera;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
